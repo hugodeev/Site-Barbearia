ed5f9724de86aaa2aa5abe531fb729cd98d1a392
Hugo Matias <hugo353637@gmail.com>
2025-01-22 16:47:42 -0300

    estrutura do formulario responsividade

--- src/js/scrpt.js
+++ src/js/scrpt.js
@@ -15,3 +15,31 @@ menuLinks.forEach(link => {
         menuHeader.classList.remove('active'); // Remove a classe 'active' para esconder o menu
     });
 });
+
+
+
+
+/*formulario botão*/
+
+function configurarFormulario(btnAgendarId, formId, btnFecharId) {
+    const btnAgendar = document.getElementById(btnAgendarId);
+    const form = document.getElementById(formId);
+    const btnFechar = document.getElementById(btnFecharId);
+
+    btnAgendar.addEventListener("click", () => {
+        form.style.display = "block";
+    });
+
+    btnFechar.addEventListener("click", () => {
+        form.style.display = "none";
+    });
+}
+
+// Configurar os 5 serviços
+configurarFormulario("btnAgendar1", "formularioAgendamento1", "btnFecharForm1");
+configurarFormulario("btnAgendar2", "formularioAgendamento2", "btnFecharForm2");
+configurarFormulario("btnAgendar3", "formularioAgendamento3", "btnFecharForm3");
+configurarFormulario("btnAgendar4", "formularioAgendamento4", "btnFecharForm4");
+configurarFormulario("btnAgendar5", "formularioAgendamento5", "btnFecharForm5");
+
+
